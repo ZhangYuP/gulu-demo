ca07b454b307849367468f56446fc01c97ca2e67
ZhangYuPeng <zyp5318@qq.com>
2019-07-18 17:10:36 +0800

    安装 chai

--- src/button-group.vue
+++ src/button-group.vue
@@ -8,7 +8,7 @@
     mounted(){
       console.log(this.$el.children);
       for (let node of this.$el.children) {
-        let name = node.nodeName.toLowerCase()
+        let name = node.nodeName.toLowerCase();
         if (name !== 'button') {
           console.warn(`g-button-group 的子元素应该全是 g-button，但是你写的是 ${name}`)
         }
