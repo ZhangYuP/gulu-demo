359fdcda149d7c2d1392252823a48635e6ebbf03
ZhangYuPeng <zyp5318@qq.com>
2019-07-22 21:49:01 +0800

    实现 position: top/middle/bottom 功能

--- src/toast.vue
+++ src/toast.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="toast" ref="toast">
+  <div class="toast" ref="toast" :class="toastClasses">
     <div class="message">
       <slot v-if="!enableHtml"></slot>
       <div v-else v-html="$slots.default[0]"></div>
@@ -34,11 +34,25 @@
       enableHtml: {
         type: Boolean,
         default: false
+      },
+      position: {
+        type: String,
+        default: 'top',
+        validator (value) {
+          return ['top', 'bottom', 'middle'].indexOf(value) >= 0
+        }
       }
     },
     mounted(){
       this.updateStyles()
       this.execAutoClose()
+    },
+    computed: {
+      toastClasses () {
+        return {
+          [`position-${this.position}`]: true
+        }
+      }
     },
     methods: {
       updateStyles () {
@@ -76,9 +90,7 @@
     min-height: $toast-min-height;
     position: fixed;
     color: white;
-    top: 0;
     left: 50%;
-    transform: translateX(-50%);
     display: flex;
     align-items: center;
     background: $toast-bg;
@@ -96,6 +108,18 @@
       height: 100%;
       border-left: 1px solid #666;
       margin-left: 16px;
+    }
+    &.position-top{
+      top: 0;
+      transform: translateX(-50%);
+    }
+    &.position-middle {
+      top: 50%;
+      transform: translate(-50%, -50%);
+    }
+    &.position-bottom {
+      bottom: 0;
+      transform: translateX(-50%);
     }
   }
 </style>
