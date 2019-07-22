951bdf54ed4d3b67ca9a74e2cc04ec7f88137b89
ZhangYuPeng <zyp5318@qq.com>
2019-07-22 22:57:21 +0800

    优化三种动画

--- src/toast.vue
+++ src/toast.vue
@@ -1,13 +1,15 @@
 <template>
-  <div class="toast" ref="toast" :class="toastClasses">
-    <div class="message">
-      <slot v-if="!enableHtml"></slot>
-      <div v-else v-html="$slots.default[0]"></div>
+  <div class="wrapper" :class="toastClasses">
+    <div class="toast" ref="toast">
+      <div class="message">
+        <slot v-if="!enableHtml"></slot>
+        <div v-else v-html="$slots.default[0]"></div>
+      </div>
+      <div class="line" ref="line"></div>
+      <span class="close" v-if="closeButton" @click="onClickClose">
+        {{closeButton.text}}
+      </span>
     </div>
-    <div class="line" ref="line"></div>
-    <span class="close" v-if="closeButton" @click="onClickClose">
-      {{closeButton.text}}
-    </span>
   </div>
 </template>
 <script>
@@ -85,18 +87,51 @@
   $font-size: 14px;
   $toast-min-height: 40px;
   $toast-bg: rgba(0,0,0,0.75);
+  @keyframes slide-down {
+    0% {opacity: 0; transform: translateY(-100%);}
+    100% {opacity: 1; transform: translateY(0%);}
+  }
+  @keyframes slide-up {
+    0% {opacity: 0; transform: translateY(100%);}
+    100% {opacity: 1; transform: translateY(0%);}
+  }
   @keyframes fade-in {
     0% {opacity: 0;}
     100% {opacity: 1;}
   }
+  .wrapper {
+    position: fixed;
+    left: 50%;
+    transform: translateX(-50%);
+    $animation-duration: 300ms;
+    &.position-top {
+      top: 0;
+      .toast {
+        border-top-left-radius: 0;
+        border-top-right-radius: 0;
+        animation: slide-down $animation-duration;
+      }
+    }
+    &.position-middle {
+      top: 50%;
+      transform: translateX(-50%) translateY(-50%);
+      animation: fade-in $animation-duration;
+    }
+    &.position-bottom {
+      bottom: 0;
+      .toast {
+        border-bottom-left-radius: 0;
+        border-bottom-right-radius: 0;
+        animation: slide-up $animation-duration;
+      }
+    }
+
+  }
   .toast {
-    animation: fade-in 1s;
     font-style: $font-size;
     line-height: 1.8;
     min-height: $toast-min-height;
-    position: fixed;
     color: white;
-    left: 50%;
     display: flex;
     align-items: center;
     background: $toast-bg;
@@ -114,18 +149,6 @@
       height: 100%;
       border-left: 1px solid #666;
       margin-left: 16px;
-    }
-    &.position-top{
-      top: 0;
-      transform: translateX(-50%);
-    }
-    &.position-middle {
-      top: 50%;
-      transform: translate(-50%, -50%);
-    }
-    &.position-bottom {
-      bottom: 0;
-      transform: translateX(-50%);
     }
   }
 </style>
