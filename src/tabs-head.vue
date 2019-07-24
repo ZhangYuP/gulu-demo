4722b22168038c196c23440f26eaaca81f1249f3
ZhangYuPeng <zyp5318@qq.com>
2019-07-24 17:14:44 +0800

    触发 update:selected 事件的时候添加一个 item 数据

--- src/tabs-head.vue
+++ src/tabs-head.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="tabs-head">
     <slot></slot>
+    <div class="line" ref="line"></div>
     <div class="actions-wrapper">
       <slot name="actions" class="actions"></slot>
     </div>
@@ -11,16 +12,28 @@
     name: 'GuluTabsHead',
     inject: ['eventBus'],
     created () {
+      this.eventBus.$on('update:selected', (item, vm) => {
+        console.log(item);
+        console.log(vm);
+      })
     }
   }
 </script>
 <style scoped lang="scss">
   $tabs-height: 40px;
+  $blue: blue;
   .tabs-head {
     height: $tabs-height;
     display: flex;
     justify-content: flex-start;
     border: 1px solid red;
+    position: relative;
+    > .line {
+      position: absolute;
+      bottom: 0;
+      border-bottom: 1px solid $blue;
+      width: 100px;
+    }
     > .actions-wrapper {
       margin-left: auto;
     }
