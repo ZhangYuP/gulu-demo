27b7ff15e4c8556e159b1c3eb97cb3a97642758c
ZhangYuPeng <zyp5318@qq.com>
2019-07-24 17:40:13 +0800

    tab 切换动画完成

--- src/tabs-head.vue
+++ src/tabs-head.vue
@@ -11,10 +11,11 @@
   export default {
     name: 'GuluTabsHead',
     inject: ['eventBus'],
-    created () {
+    mounted () {
       this.eventBus.$on('update:selected', (item, vm) => {
-        console.log(item);
-        console.log(vm);
+        let {width, height, top, left} = vm.$el.getBoundingClientRect()
+        this.$refs.line.style.width = `${width}px`
+        this.$refs.line.style.left = `${left}px`
       })
     }
   }
@@ -26,13 +27,12 @@
     height: $tabs-height;
     display: flex;
     justify-content: flex-start;
-    border: 1px solid red;
     position: relative;
     > .line {
       position: absolute;
       bottom: 0;
       border-bottom: 1px solid $blue;
-      width: 100px;
+      transition: all 350ms;
     }
     > .actions-wrapper {
       margin-left: auto;
