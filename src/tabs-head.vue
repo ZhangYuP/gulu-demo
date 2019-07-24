686eb1b9f7dbcdd9c7d317a72dd87e4a7b2747d7
ZhangYuPeng <zyp5318@qq.com>
2019-07-24 16:28:46 +0800

    tabs 可以切换了

--- src/tabs-head.vue
+++ src/tabs-head.vue
@@ -1,16 +1,30 @@
 <template>
   <div class="tabs-head">
     <slot></slot>
-    <slot name="actions"></slot>
+    <div class="actions-wrapper">
+      <slot name="actions" class="actions"></slot>
+    </div>
   </div>
 </template>
 <script>
   export default {
-    name: 'GuluTabsHead'
+    name: 'GuluTabsHead',
+    inject: ['eventBus'],
+    created () {
+      console.log(this.eventBus);
+    }
   }
 </script>
-<style>
+<style scoped lang="scss">
+  $tabs-height: 40px;
   .tabs-head {
-    
+    height: $tabs-height;
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    border: 1px solid red;
+    > .actions-wrapper {
+      margin-left: auto;
+    }
   }
 </style>
--- src/tabs.vue
+++ src/tabs.vue
@@ -4,6 +4,8 @@
   </div>
 </template>
 <script>
+  import Vue from 'vue';
+
   export default {
     name: 'GuluTabs',
     props: {
@@ -18,6 +20,19 @@
           return ['horizontal', 'vertical'].indexOf(value) >= 0
         }
       }
+    },
+    data () {
+      return {
+        eventBus: new Vue()
+      }
+    },
+    provide () {
+      return {
+        eventBus: this.eventBus
+      }
+    },
+    mounted () {
+      this.eventBus.$emit('update:selected', this.selected)
     }
   }
 </script>
